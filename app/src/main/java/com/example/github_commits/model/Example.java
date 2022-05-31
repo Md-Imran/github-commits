ce5e80c72d4ef9fef74cb06888c5e477f1d2ca25
Imran11 <Annex@110211@>
2022-05-31 23:01:44 +0600

    git init

---
+++ app/src/main/java/com/example/github_commits/model/Example.java
@@ -0,0 +1,109 @@
+package com.example.github_commits.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Example {
+
+    @SerializedName("sha")
+    @Expose
+    private String sha;
+    @SerializedName("node_id")
+    @Expose
+    private String nodeId;
+    @SerializedName("commit")
+    @Expose
+    private Commit commit;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("html_url")
+    @Expose
+    private String htmlUrl;
+    @SerializedName("comments_url")
+    @Expose
+    private String commentsUrl;
+    @SerializedName("author")
+    @Expose
+    private Author__1 author;
+    @SerializedName("committer")
+    @Expose
+    private Committer__1 committer;
+    @SerializedName("parents")
+    @Expose
+    private List<Parent> parents = null;
+
+    public String getSha() {
+        return sha;
+    }
+
+    public void setSha(String sha) {
+        this.sha = sha;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public Commit getCommit() {
+        return commit;
+    }
+
+    public void setCommit(Commit commit) {
+        this.commit = commit;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    public String getCommentsUrl() {
+        return commentsUrl;
+    }
+
+    public void setCommentsUrl(String commentsUrl) {
+        this.commentsUrl = commentsUrl;
+    }
+
+    public Author__1 getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author__1 author) {
+        this.author = author;
+    }
+
+    public Committer__1 getCommitter() {
+        return committer;
+    }
+
+    public void setCommitter(Committer__1 committer) {
+        this.committer = committer;
+    }
+
+    public List<Parent> getParents() {
+        return parents;
+    }
+
+    public void setParents(List<Parent> parents) {
+        this.parents = parents;
+    }
+
+}
